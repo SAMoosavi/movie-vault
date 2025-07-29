83a0ba7793b664648f3c835669ebf3840b2da3c3
S.A.Moosavi <moosavi238@gmail.com>
2025-07-29 17:35:01 +0330

    write all invoke

---
+++ src/functions/invoker.ts
@@ -0,0 +1,14 @@
+import type { FilterValues, VideoMetaData } from '../type'
+import { invoke } from '@tauri-apps/api/core'
+
+export async function create_table() { await invoke('create_table_app') }
+
+export async function sync_app(dir: string) { await invoke('sync_app_files', { root: dir, apiKey: '4c602a26' }) }
+
+export async function get_all_video_metadata() { return await invoke<VideoMetaData[]>('get_all_video_metadata_app') }
+
+export async function get_genres() { return await invoke<[number, string][]>('get_genres_app') }
+
+export async function get_countries() { return await invoke<[number, string][]>('get_countries_app') }
+
+export async function search_videos(filters: FilterValues) { return await invoke<VideoMetaData[]>('search_videos_app', { filters }) }
