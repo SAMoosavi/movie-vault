b92c9dc6af962fa3f0c32c7b0bc1752c83a868d9
S.A.Moosavi <moosavi238@gmail.com>
2025-08-16 23:07:57 +0330

    add tag manager

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,6 +2,33 @@
   <main class="container mx-auto px-4 py-6">
     <FilterMovies />
 
+    <!-- Tag Manager -->
+    <div class="mb-3 rounded-xl border-2 p-3">
+      <div class="flex items-center mb-2 gap-3">
+        <TagIcon class="h-6 w-6" />
+        <h3 class="text-xl">tag manager</h3>
+      </div>
+      <div class="flex items-center">
+        <!-- Add Tag -->
+        <div class="flex basis-1/3 gap-3">
+          <input class="input" v-model="tag.name" />
+          <button class="btn" @click="addTag">add tag</button>
+        </div>
+
+        <!-- Show and remove tag -->
+        <div class="flex flex-wrap gap-1">
+          <span
+            v-for="tag in tags"
+            :key="tag.id"
+            class="badge badge-outline badge-sm cursor-pointer"
+            @click="removeTag(tag.id)"
+          >
+            {{ tag.name }}
+          </span>
+        </div>
+      </div>
+    </div>
+
     <LoadingView v-if="loading" />
     <!-- Movie Grid -->
     <div v-else>
@@ -26,6 +53,9 @@ import MovieCard from '../component/MovieCard.vue'
 import { useVideosStore } from '../stores/Videos'
 import { storeToRefs } from 'pinia'
 import { useFiltersStore } from '../stores/Filters'
+import type { Tag } from '../type'
+import { get_tags, insert_tag, remove_tag } from '../functions/invoker'
+import { TagIcon } from 'lucide-vue-next'
 
 const loading = ref(true)
 
@@ -33,6 +63,8 @@ const videos = useVideosStore()
 const { videos_metadata } = storeToRefs(videos)
 
 onMounted(async () => {
+  await getTags()
+
   try {
     await videos.updata()
 
@@ -59,5 +91,23 @@ async function search() {
     .then(() => {})
     .catch((e) => toast.error(e))
     .finally(() => (loading.value = false))
+}
+
+const tags = ref<Tag[]>([])
+const tag = ref<Tag>({ id: 0, name: '' })
+
+async function getTags() {
+  tags.value = await get_tags()
+}
+
+async function addTag() {
+  await insert_tag(tag.value)
+  tag.value = { id: 0, name: '' }
+  await getTags()
+}
+
+async function removeTag(tag_id: number) {
+  await remove_tag(tag_id)
+  await getTags()
 }
 </script>
