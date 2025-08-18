06dc48c1d0f44e0153edc345a3001a9520088d3d
SAMoosavi <moosavi238@gmail.com>
2025-08-18 16:12:48 +0330

    add tauri-plugin-sql package

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -179,6 +179,7 @@ fn remove_media_tag(media_id: i64, tag_id: i64) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
